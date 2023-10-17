dd944ac6c3b879a9d4d35cecdd368ceb887becd5
Tshembani04 <aurthurmhlongo.ta@gmail.com>
2023-10-17 23:39:37 +0200

    second

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,10 @@
-// "use client";
+"use client";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./Firebase";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 //Set an authentication state observer and get user data using onAuthStateChange
 
@@ -12,6 +16,20 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const uid = user.uid;
+        console.log("Logged in userID:", uid);
+        router.push("/home");
+      } else {
+        router.push("/login");
+      }
+    });
+  }, []);
+
   return (
     <>
       <html lang="en">
