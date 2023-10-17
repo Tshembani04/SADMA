bee1ee7d1559e402c5b497adc470c41d4082cdde
Tshembani04 <aurthurmhlongo.ta@gmail.com>
2023-10-17 23:01:49 +0200

    clean up

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,4 @@
 "use client";
-import "./globals.css";
 import { Inter } from "next/font/google";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./Firebase";
@@ -33,7 +32,7 @@ export default function RootLayout({ children }) {
   return (
     <>
       <html lang="en">
-        <body className={inter.className}> {children} </body>
+        <body className={inter.className}>{children}</body>
       </html>
     </>
   );
