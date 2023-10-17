8b0e84a26d7a71154b4c2c961aefa73fb8941ac0
Tshembani04 <aurthurmhlongo.ta@gmail.com>
2023-10-17 23:09:29 +0200

    clean up

---
+++ src/components/HomeHolder.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from "react";
+import Header from "./header";
+import TopCards from "./topcards";
+
+function HomeHolder() {
+  return (
+    <>
+      <Header />
+      <TopCards />
+    </>
+  );
+}
+
+export default HomeHolder;
